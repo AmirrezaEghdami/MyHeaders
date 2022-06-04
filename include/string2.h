fa8458d2764667843943b7e4e9fa60d1151a524a
Amirreza <eghdamiamirreza@gmail.com>
2022-06-04 17:42:25 +0430

    Rename variables, Add .gitignore

--- include/string2.h
+++ include/string2.h
@@ -2,19 +2,19 @@
 #include <string.h>
 
 int isAinB(char *a, char *b){
-	int n = 0;
+	int counter = 0;
 	int aLength = strlen(a);
 	int bLength = strlen(b);
 	for (int i = 0; i < bLength; i++){
-		if (a[0+n] == b[i]){
-			n = n + 1;
-			if (n == aLength){
+		if (a[0+counter] == b[i]){
+			counter = counter + 1;
+			if (counter == aLength){
 				printf("%s is in %s",a,b);
 				return 1;
 			}
 		}
 		else{
-			n = 0;
+			counter = 0;
 		}
 	}
 	printf("%s is not in %s",a,b);
@@ -22,24 +22,24 @@ int isAinB(char *a, char *b){
 }
 
 int hideAfromB(char *a, char *b){
-	int n = 0;
+	int counter = 0;
 	int aLength = strlen(a);
 	int bLength = strlen(b);
 	char newB[bLength];
 	strcpy(newB, b);
 	for (int i = 0; i < bLength; i++){
-		if (a[0+n] == newB[i]){
-			n = n + 1;
-			if (n == aLength){
+		if (a[0+counter] == newB[i]){
+			counter = counter + 1;
+			if (counter == aLength){
 				for (int j = 0; j < aLength; j++)
 				{
 					newB[i-j] = '*';
 				}
-				n = 0;
+				counter = 0;
 			}
 		}
 		else{
-			n = 0;
+			counter = 0;
 		}
 	}
 	printf("%s",newB);
--- main.c
+++ main.c
@@ -6,5 +6,8 @@
 
 int main()
 {
+	/*
+	Here is for debugging functions in header files
+	*/
 	return 0;
 }
