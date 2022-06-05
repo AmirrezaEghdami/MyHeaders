8cdfede5ef8842e27f7d861713fe1d29a5c56331
Amirreza <eghdamiamirreza@gmail.com>
2022-06-05 09:45:56 +0430

    Change variable name

--- include/number.h
+++ include/number.h
@@ -2,13 +2,13 @@
 
 void printAtoB(int a, int b){
     if (a < b){
-        for (int i = a; a <= b; a++){
-            printf("%d\n",a);
+        for (int i = a; i <= b; i++){
+            printf("%d\n",i);
         }
     }
     else if(a > b){
-        for (int i = a; a >= b; a--){
-            printf("%d\n",a);
+        for (int i = a; i >= b; i--){
+            printf("%d\n",i);
         }
     } else {
         printf("%d\n",a);
