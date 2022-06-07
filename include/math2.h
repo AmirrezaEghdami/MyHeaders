8502184515df54bd02fbe9d52d45522a56cfa747
Amirreza Eghdami <eghdamiamirreza@gmail.com>
2022-06-07 10:47:25 -0700

    'math2.h' added & add the 'fibonacci' function to it

---
+++ include/math2.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void fibonacci(int firstNnumbers){
+    int x, y, z;
+	int firstNnumbers;
+	x = 0;
+	y = 1;
+	for (int i = 0; i < firstNnumbers; i++){
+		if (i <= 1){
+			z = i;
+		}
+		else{
+			z = x + y;
+			x = y;
+			y = z;
+		}
+		printf("%d\n",z);
+	}
+}
